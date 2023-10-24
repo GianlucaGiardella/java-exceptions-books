14c5eaad751e6203e07d11672e36bdce61df8e97
GianluGiarde <gianlugiarde@gmail.com>
2023-10-24 16:52:50 +0200

    add main & book

--- src/org/lessons/java/books/Book.java
+++ src/org/lessons/java/books/Book.java
@@ -1,4 +1,74 @@
-package org.lessons.java;
+package org.lessons.java.books;
 
 public class Book {
+    private String title;
+    private int pages;
+    private String author;
+    private String editor;
+
+    public Book(String title, int pages, String author, String editor) throws IllegalArgumentException {
+        validateTitle(title);
+        validatePages(pages);
+        validateAuthor(author);
+        validateEditor(editor);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public int getPages() {
+        return pages;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setTitle(String title) {
+        validateTitle(title);
+    }
+    public void setPages(int pages) {
+        validatePages(pages);
+    }
+    public void setAuthor(String author) {
+        validateAuthor(author);
+    }
+    public void setEditor(String editor) {
+        validateEditor(editor);
+    }
+
+    private void validateTitle(String title) {
+        if (title == null || title.trim().isEmpty()) {
+            throw new IllegalArgumentException("Titolo non valido");
+        }
+        this.title = title;
+    }
+    private void validatePages(int pages) {
+        if (pages < 1) {
+            throw new IllegalArgumentException("Numero pagine maggiore di 0");
+        }
+        this.pages = pages;
+    }
+    private void validateAuthor(String author) {
+        if (author == null || author.trim().isEmpty()) {
+            throw new IllegalArgumentException("Autore non valido");
+        }
+        this.author = author;
+    }
+    private void validateEditor(String editor) {
+        if (editor == null || editor.trim().isEmpty()) {
+            throw new IllegalArgumentException("Editore non valido");
+        }
+        this.editor = editor;
+    }
+
+    @Override
+    public String toString() {
+        return "title: " + title + ", " +
+                "pages: " + pages + ", " +
+                "author: " + author + ", " +
+                "editor: " + editor + ", ";
+    }
 }
--- src/org/lessons/java/books/Main.java
+++ src/org/lessons/java/books/Main.java
@@ -1,7 +1,67 @@
-package org.lessons.java;
+package org.lessons.java.books;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        int numberOfBooks = 0;
+        boolean isValid = false;
+
+        do {
+            try {
+                System.out.println("Inserire il numero di libri: ");
+                numberOfBooks = Integer.parseInt(scan.nextLine());
+
+                if (numberOfBooks > 0) {
+                    isValid = true;
+                } else {
+                    System.out.println("- Il numero deve essere maggiore di 0!");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("- Numero non valido!");
+            }
+        } while (!isValid);
+
+        Book[] books = new Book[numberOfBooks];
+
+        for (int i = 0; i < numberOfBooks; i++) {
+            System.out.println("\n- Libro N." + (i + 1));
+
+            boolean areValid = false;
+
+            do {
+                try {
+                    System.out.println("Inserire il titolo: ");
+                    String title = scan.nextLine();
+
+                    System.out.println("Inserire il numero di pagine: ");
+                    int pages = Integer.parseInt(scan.nextLine());
+
+                    System.out.println("Inserire l'autore: ");
+                    String author = scan.nextLine();
+
+                    System.out.println("Inserire l'editore: ");
+                    String editor = scan.nextLine();
+
+                    books[i] = new Book(title, pages, author, editor);
+                    areValid = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("Numero non valido");
+                } catch (IllegalArgumentException e) {
+                    System.out.println("- Errore: " + e.getMessage());
+                }
+            } while (!areValid);
+
+            System.out.println("- Libro creato con successo!");
+        }
+
+        System.out.println("\n- Libri creati: ");
+        for (Book book : books) {
+            System.out.println(book);
+        }
+
+        scan.close();
     }
 }
